574223e4b09bd4383bb42139b5064a78ae7445d5
Robertpi1 <40431036+robert1chi@users.noreply.github.com>
2022-01-28 12:21:57 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
-export const i18n = {
-  locales: ["en-US", "zh-CN", "ja"],
-  defaultLocale: "en-US",
-}
-export const reactStrictMode = true
-export async function generateBuildId() {
-  return 'v1'
+module.exports = {
+  i18n: {
+    locales: ["en-US", "zh-CN", "ja"],
+    defaultLocale: "en-US",
+  },
+  reactStrictMode: true
 }
